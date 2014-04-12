cacb6f766344b47882057f08b1e8592335338eb9
Yonah Forst <joshblour@hotmail.com>
2014-04-12 18:12:49 +0200

    added article views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,51 @@
 class ArticlesController < ApplicationController
+  load_and_authorize_resource
+  
   def index
-    @articles = Article.all
   end
+  
+  def show
+  end
+  
+  def new
+  end
+  
+  def create    
+    respond_to do |format|
+      if @article.save
+        format.html
+        format.json { render json: @article }
+      else
+        format.html
+        format.json
+      end
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    respond_to do |format|
+      if @article.update_attribtues(article_params)
+        format.html
+        format.json { render json: @article }
+      else
+        format.html
+        format.json
+      end
+    end
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  
+  
 end
