9e206ecfd8febf656ec04ced881b4e0a61260ca7
Bernard Schembri <bernard@webgenix.com>
2014-04-12 18:59:17 +0200

    added json api for single location

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ ActiveRecord::Base.transaction do
   article3 = u.articles.create!(title: 'Bar hopping in Gracia', content: 'Bar hopping in gracia.  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?')
 
   article1.locations = [Location.create!(name: 'Betahaus', latitude: 41.4069647, longitude: 2.1568569)]
-  article1.locations = [Location.create!(name: 'Can Tresó', latitude: 41.402982, longitude: 2.153739)]
+  article2.locations = [Location.create!(name: 'Can Tresó', latitude: 41.402982, longitude: 2.153739)]
   article3.locations  =[ Location.create!(name: 'Muntanya de Montjuïc', latitude: 41.3641667, longitude: 2.1580556),
                          Location.create!(name: 'Tibidabo', latitude: 41.4209857, longitude: 2.1187937)]
 
