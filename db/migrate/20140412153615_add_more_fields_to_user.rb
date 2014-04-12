0d30b31a525c1751aa7a8dfcd52383bda5034e7c
Yonah Forst <joshblour@hotmail.com>
2014-04-12 17:43:05 +0200

    re-added fields to users

--- db/migrate/20140412153615_add_more_fields_to_user.rb
+++ db/migrate/20140412153615_add_more_fields_to_user.rb
@@ -1,4 +1,4 @@
-class AddFieldsToUsers < ActiveRecord::Migration
+class AddMoreFieldsToUser < ActiveRecord::Migration
   def change
     add_column :users, :profession, :string
     add_column :users, :nationality, :string
