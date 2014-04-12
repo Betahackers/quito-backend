109d6aa3a72e4ec138abd5eac908515fe60bc435
Leif Gensert <leif@propertybase.com>
2014-04-12 13:31:41 +0200

    add article model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412100132) do
+ActiveRecord::Schema.define(version: 20140412110731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
---
+++ spec/models/article_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Article do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  context '#valid' do
+    describe '#title' do
+      subject { FactoryGirl.create!(:article).title }
+
+      it { is_expected.not_to be_nil }
+    end
+  end
+end
