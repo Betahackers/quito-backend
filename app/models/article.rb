3e051fba18d4110a05293188eb0816d216d2850d
Bernard Schembri <bernard@webgenix.com>
2014-04-12 17:44:39 +0200

    add article -> locations relationship

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
   belongs_to :user
 
+  has_many :locations
+
   def moods
     ['Illegal','Sociable','Adventure','Active','Cultural','Romantic','Relaxed','Solitary'].sample(rand(3..5))
   end
