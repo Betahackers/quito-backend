b3a797b913e74ac1f9cea64849a0b2c6ea15c12f
Leif Gensert <leif@propertybase.com>
2014-04-12 17:46:13 +0200

    add test for creating location from foursquare_hash

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,12 @@
 class Location < ActiveRecord::Base
   reverse_geocoded_by :latitude, :longitude
+
+  def self.new_by_foursquare_hash hash
+    create!(
+      foursquare_id: hash['id'],
+      name: hash['name'],
+      latitude: hash['location']['lat'],
+      longitude: hash['location']['lng'],
+    )
+  end
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Location do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.new_by_foursquare_hash' do
+    before(:each) do
+      described_class.new_by_foursquare_hash(foursquare_hash)
+    end
+
+    context '#valid' do
+      let(:foursquare_hash) { FactoryGirl.build(:foursquare_hash) }
+      let(:loaded_object) { Location.find_by_foursquare_id(foursquare_hash['id']) }
+
+      it 'is found by foursquare_id' do
+        expect(loaded_object).not_to be_nil
+      end
+
+      it 'has all fields filled' do
+        expect(loaded_object.id).not_to be_nil
+        expect(loaded_object.name).not_to be_nil
+        expect(loaded_object.latitude).not_to be_nil
+        expect(loaded_object.longitude).not_to be_nil
+        expect(loaded_object).to be_geocoded
+      end
+    end
+  end
 end
