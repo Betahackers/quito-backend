6dd389e4febd0de36c6128b67c74c6d72e7f3ecb
Yonah Forst <joshblour@hotmail.com>
2014-04-12 13:45:14 +0200

    added user fields

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'admin@admin.com', password: 'Ilovebetahaus', first_name: 'P', last_name: 'ennis')
