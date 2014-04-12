3a151a4f204dbaf6f0f0b84762b7b8adb7e71fec
Bernard Schembri <bernard@webgenix.com>
2014-04-12 14:30:19 +0200

    Added presence validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+
+  validates_presence_of :role
+
 end
