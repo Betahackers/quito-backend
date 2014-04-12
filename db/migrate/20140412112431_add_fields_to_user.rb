4bac3cbcc6b1b4ba18ba7a65caf2462317ba6d6d
Yonah Forst <joshblour@hotmail.com>
2014-04-12 13:46:12 +0200

    forgot to add file

---
+++ db/migrate/20140412112431_add_fields_to_user.rb
@@ -0,0 +1,8 @@
+class AddFieldsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :role, :string
+    add_column :users, :about, :text
+  end
+end
