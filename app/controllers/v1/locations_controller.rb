71db201a430d2267d47158436e08e958026a9933
Leif Gensert <leifg@gmx.de>
2014-04-13 15:54:46 +0200

    add locations controller

--- app/controllers/v1/locations_controller.rb
+++ app/controllers/v1/locations_controller.rb
@@ -3,7 +3,12 @@ module V1
     load_and_authorize_resource
 
     def index
-      Location.all
+      if params[:ll]
+        @locations = Location.near(params[:ll], params[:radius].to_f / 1000, units: :km)
+      else
+        @locations = Location.all
+      end
+      @locations
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,5 +18,6 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
   config.order = 'random'
+  config.render_views = true
   config.include Devise::TestHelpers, type: :controller
 end
