8d2bc1241aeb479d1b593f788b738dde1ceddf4f
Bernard Schembri <bernard@webgenix.com>
2014-04-13 16:13:57 +0200

    rescuing access denied; updated nav bar with sign out link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to new_user_session_path, flash: { error: "Please log in first." }
+
+  end
   
 end
