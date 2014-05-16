cffb9c8ed68500368c4a85430163ca522f0e75e6
Yonah Forst <joshblour@hotmail.com>
2014-05-16 10:12:10 +0200

    update mailer onfig

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
       :port => 587, # ports 587 and 2525 are also supported with STARTTLS
       :enable_starttls_auto => true, # detects and uses STARTTLS
       :user_name => "#{ENV['MANDRILL_USERNAME']}",
-      :password => "#{ENV['MANDRILL_PASSWORD']}", # SMTP password is any valid API key
+      :password => "#{ENV['MANDRILL_APIKEY']}", # SMTP password is any valid API key
       :authentication => 'login', # Mandrill supports 'plain' or 'login'
       :domain => 'www.fromto.es', # your domain to identify your server when connecting
   }
